d5a3bb6e7c7b0fb548c66db9c176d24d2d7dcb40
paterdimitry <paterdimitry@gmail.com>
2021-04-30 12:35:09 +0300

    Котлин. Урок 1

---
+++ app/src/main/java/com/geekbrain/kotlinbase/Cat.kt
@@ -0,0 +1,3 @@
+package com.geekbrain.kotlinbase
+
+data class Cat(val name: String, val age: Int, val color: String)
--- app/src/main/java/com/geekbrain/kotlinbase/MainActivity.kt
+++ app/src/main/java/com/geekbrain/kotlinbase/MainActivity.kt
@@ -1,14 +1,43 @@
-package com.geekbrain.kotlinbase;
+package com.geekbrain.kotlinbase
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
-import android.os.Bundle;
+class MainActivity : AppCompatActivity() {
 
-public class MainActivity extends AppCompatActivity {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initView()
+    }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    private fun initView() {
+        val textView: TextView = findViewById(R.id.textView)
+        val cats: List<Cat> = initCatList()
+        initButton(cats, textView)
+    }
+
+    private fun initCatList(): List<Cat> {
+        val cat1 = Cat("Barsik", 12, "red")
+        val cat2 = Cat("Myrsik", 6, "white")
+        return listOf(cat1, cat2)
+    }
+
+    private fun initButton(cats: List<Cat>, textView: TextView) {
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            var text = ""
+            for (cat in cats) {
+                val color = when (cat.color) {
+                    "red" -> "Красный"
+                    "white" -> "Белый"
+                    else -> "Неизвестный"
+                }
+                text += "Кот по имени ${cat.name} возраста ${cat.age} лет и его цвет - $color \n"
+            }
+            textView.text = text
+        }
     }
 }
