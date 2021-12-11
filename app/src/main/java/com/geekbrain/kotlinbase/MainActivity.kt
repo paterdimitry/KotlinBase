754b03f5e0ba0c28ef4eb94152bbd531622d943f
paterdimitry <paterdimitry@gmail.com>
2021-12-11 14:25:15 +0300

    Lesson 1

--- app/src/main/java/com/geekbrain/kotlinbase/MainActivity.kt
+++ app/src/main/java/com/geekbrain/kotlinbase/MainActivity.kt
@@ -16,13 +16,16 @@ class MainActivity : AppCompatActivity() {
     private fun initView() {
         val textView: TextView = findViewById(R.id.textView)
         val cats: List<Cat> = initCatList()
+
+        printCatName(cats)
+        colorCat(cats)
         initButton(cats, textView)
     }
 
     private fun initCatList(): List<Cat> {
         val cat1 = Cat("Barsik", 12, "red")
         val cat2 = Cat("Myrsik", 6, "white")
-        return listOf(cat1, cat2)
+        return  listOf(cat1, cat2)
     }
 
     private fun initButton(cats: List<Cat>, textView: TextView) {
@@ -39,5 +42,16 @@ class MainActivity : AppCompatActivity() {
             }
             textView.text = text
         }
+    }
+
+    private fun printCatName(cats: List<Cat>) {
+        for(cat in cats) {
+            println("Имя кота " + cat.name)
+        }
+    }
+
+    private fun colorCat(cats: List<Cat>) {
+        val cat = cats[0].copy(color = "black")
+        println("Кот по имени ${cat.name} возраста ${cat.age} лет и его цвет - ${cat.color} \n")
     }
 }
